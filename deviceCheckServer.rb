557b62402f9eebcb12cfb0028b5d6d573d75e476
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-09-28 16:56:46 +0900

    [ADD] ios 루비 API  연결

--- deviceCheckServer.rb
+++ deviceCheckServer.rb
@@ -3,18 +3,28 @@ require 'http'
 require 'jwt'
 require 'securerandom'
 
+
+get '/' do  
+  "Please send the base 64 encoded device check token in JSON parameter key 'token' to POST /redeem"
+  
+end
+
+
 def jwt_token
   # use the content of the .p8 key file you downloaded, it should look like this :
   #-----BEGIN PRIVATE KEY-----
   #ILIKEFOXES
   #-----END PRIVATE KEY----
-  private_key = ENV['DEVICE_CHECK_KEY_STRING']
+  private_key = ENV['MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgXUOrlSzA/DYC3n+G
+    OldH2Goyfd4KerNn1LXg96LQx4qgCgYIKoZIzj0DAQehRANCAASCRIcUQy7Jg/Vr
+    T/lryBmhz42ADSCmZq4QAJpWKr/Y8Ulji8000CrSlIk4uTL5hjAKZO9tIW8Fj3n7
+    KZ8617VQ']
   
   # the Key ID you saw earlier
-  key_id = ENV['DEVICE_CHECK_KEY_ID']
+  key_id = ENV['WL379Z2K7F']
   
   # Team ID of your Apple developer account
-  team_id = ENV['DEVICE_CHECK_TEAM_ID']
+  team_id = ENV['ELAND RETAIL., Ltd.']
 
   # Elliptic curve key, used to encrypt the JWT
   ec_key = OpenSSL::PKey::EC.new(private_key)
